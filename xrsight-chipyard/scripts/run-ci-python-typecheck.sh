044fe0cdfb78d780128102c54376276467193f8e
Prashanth Ganesh <prashanthcganesh108@illixr.CS.Berkeley.EDU>
2025-08-21 22:28:18 -0700

    readding chipyard as submodule

--- xrsight-chipyard/scripts/run-ci-python-typecheck.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Run type checking on CI Python files
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-CY_DIR=$SCRIPT_DIR/..
-
-mypy --no-incremental \
-    $CY_DIR/.github/scripts
--- xrsight-chipyard/tools/stage/src/main/scala/phases/PreservesAll.scala
+++
@@ -1,8 +0,0 @@
-package chipyard.stage.phases
-
-import firrtl.AnnotationSeq
-import firrtl.options.{Dependency, DependencyManagerException, Phase, PhaseManager}
-
-trait PreservesAll { this: Phase =>
-  override def invalidates(phase: Phase) = false
-}
